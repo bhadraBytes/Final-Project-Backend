3baf3171bc61a7d8bd908ce0f00e7ae20741dcb6
bhadraBytes <yashTechMail@proton.me>
2024-05-17 17:23:55 +0530

    changes into the index.js in session cookie thing

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ server.use(
     secret: process.env.SESSION_KEY,
     resave: false, // don't save session if unmodified
     saveUninitialized: false, // don't create session until something stored
-    cookie: { httpOnly: true, secure: process.env.NODE_ENV === 'production' },
+    cookie: { httpOnly: true },
   })
 );
 server.use(passport.authenticate("session"));
